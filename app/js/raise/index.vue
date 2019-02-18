13e40ed8f2d22b88c08cc6a9aff8919a1d081a5c
zyx <1113470466@qq.com>
2019-02-18 22:15:10 +0800

    白条页框架

---
+++ app/js/raise/index.vue
@@ -0,0 +1,26 @@
+<template lang="html">
+    <div>
+        <Heador/>
+        <!-- <Slider/> -->
+        <Footor/>
+        <Navbar/>
+    </div>
+</template>
+
+<script>
+import Heador from "../public/header.vue"
+import Footor from "../public/footer.vue"
+import Navbar from "../public/navbar.vue"
+// import Slider from "./rslider.vue"
+
+export default {
+    components: {
+        Heador,
+        Footor,
+        Navbar,
+        // Slider,
+    },
+}
+</script>
+<style lang="scss">
+</style>
